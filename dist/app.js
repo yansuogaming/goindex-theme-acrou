f7f720b31de6b0710125c7245e20d295dc463a85
yansuogaming <yansuogaming@gmail.com>
2024-10-23 16:49:44 +0700

    Update Language Vietnamese and Update JS

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,18 @@
 var scripts = [
-  "https://cdn.jsdelivr.net/gh/Aicirou/goindex-theme-acrou@v2.0.8/dist/js/app.da7f5f3f.js",
-  "https://cdn.jsdelivr.net/gh/Aicirou/goindex-theme-acrou@v2.0.8/dist/js/chunk-vendors.3c185c97.js",
+  "https://cdn.jsdelivr.net/gh/Aicirou/goindex-theme-acrou/dist/js/app.da7f5f3f.js",
+  "https://cdn.jsdelivr.net/gh/Aicirou/goindex-theme-acrou/dist/js/chunk-vendors.3c185c97.js",
 ];
 var cdnjs = [
-  "https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js",
-  "https://cdn.jsdelivr.net/npm/vue-i18n@8.17.3/dist/vue-i18n.min.js",
-  "https://cdn.jsdelivr.net/npm/vue-router@3.1.6/dist/vue-router.min.js",
-  "https://cdn.jsdelivr.net/npm/vuex@3.4.0/dist/vuex.js",
-  "https://cdn.jsdelivr.net/npm/axios@0.19.2/dist/axios.min.js",
-  "https://cdn.jsdelivr.net/npm/element-ui@2.13.1/lib/index.js",
-  "https://cdn.jsdelivr.net/npm/lodash@4.17.15/lodash.min.js",
-  "https://cdn.jsdelivr.net/npm/js-cookie@2.2.1/src/js.cookie.min.js",
-  "https://cdn.jsdelivr.net/npm/lowdb@1.0.0/dist/low.min.js",
-  "https://cdn.jsdelivr.net/npm/lowdb@1.0.0/dist/LocalStorage.min.js",
+  "https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js",
+  "https://cdn.jsdelivr.net/npm/vue-i18n/dist/vue-i18n.min.js",
+  "https://cdn.jsdelivr.net/npm/vue-router/dist/vue-router.min.js",
+  "https://cdn.jsdelivr.net/npm/vuex/dist/vuex.js",
+  "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js",
+  "https://cdn.jsdelivr.net/npm/element-ui/lib/index.js",
+  "https://cdn.jsdelivr.net/npm/lodash/lodash.min.js",
+  "https://cdn.jsdelivr.net/npm/js-cookie/src/js.cookie.min.js",
+  "https://cdn.jsdelivr.net/npm/lowdb/dist/low.min.js",
+  "https://cdn.jsdelivr.net/npm/lowdb/dist/LocalStorage.min.js",
   "https://at.alicdn.com/t/font_1760192_8add3xcxs4u.js",
 ];
 cdnjs.forEach((item) => {
